39a52935879ffa2af2ec03656c8d8eb486b0c5ae
chaixiang2002 <973731820@qq.com>
2021-11-27 17:22:17 +0000

    _

---
+++ data_structures/graph_List12/try/List.cpp
@@ -0,0 +1,14 @@
+#include "List.hpp"
+#include <iostream>
+// Actual parameters:       _A
+// Formal parameters:       _F
+using namespace std;
+int main(){
+    Elemtype Vertices_A[]={'A'};
+    int num_Vertices_A=6;
+    ListGraph Graph_List;
+    AdjInit(&Graph_List);
+   
+
+
+}
--- pull.sh
+++ pull.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git pull origin master
+git pull origin master 
